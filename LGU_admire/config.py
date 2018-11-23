e6a4a72cf25b7f02d186d4e85c5cf217f8710aaf
Brando Zhang <116010308@link.cuhk.edu.cn>
2018-11-23 23:40:42 +0800

    Fix: typo

--- LGU_admire/config.py
+++ LGU_admire/config.py
@@ -14,4 +14,4 @@ dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'a sample secret string')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATASET_URI = os.getenv('DATABASE_URI', dev_db)
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
